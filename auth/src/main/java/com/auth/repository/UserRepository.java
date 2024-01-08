cae208a0c57aa8c0c5edf7de81bbe6b311cbc537
Oleksandr <saharek123123@gmail.com>
2024-01-08 19:57:20 +0200

    little fix

--- auth/src/main/java/com/auth/repository/UserRepository.java
+++ auth/src/main/java/com/auth/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.auth.models.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
   Optional<User> findByUsername(String username);
 
   Boolean existsByUsername(String username);
